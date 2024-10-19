2ea02dd8be89a6532981244d881b029167dad4ad
xden78 <venera.opt@gmail.com>
2024-10-19 23:06:08 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,38 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        byte leapYear;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                    leapYear = 1;
+        }
+        else leapYear = 0;
 
+        switch (month) {
+            case (1):
+            case (3):
+            case (5):
+            case (7):
+            case (8):
+            case (10):
+            case (12):
+                System.out.println(31);
+                break;
+            case (4):
+            case (6):
+            case (9):
+            case (11):
+                System.out.println(30);
+                break;
+            case (2): {
+                if (leapYear == 1) {
+                    System.out.println(29);
+                }
+                else System.out.println(28);
+                break;
+            }
+            default:
+                System.out.println("invalid date");
+                break;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        // and will perform integer division of dividend be divider, save the value,
+        // and then will multiply result by divider and will if got value is equal
+        // to dividend will print:"can be divided completely",
+        // otherwise "cannot be divided completely"
+        // or "division by zero".
+        if (divider == 0) {
+            System.out.println("division by zero");
+            return;
+        }
+        float integerDivision = (float) dividend / divider;
+        if ((integerDivision * divider) == dividend) System.out.println("can be divided completely");
+        else System.out.println("cannot be divided completely");
     }
 }
