17f4469eb49803c704cad956edbcbae088377453
xden78 <venera.opt@gmail.com>
2024-10-19 23:43:15 +0300

    5

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
-        // Swawap(first, second);
+        first ^= second;
+        second ^= first;
+        first ^= second;
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.finalTask;
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
     // output when correct: "this is a valid triangle", otherwise : "it's not a triangle")
-        if (((float)firstSide == (float)secondSide) && ((float)secondSide == (float)thirdSide)) {
+        if (((int)firstSide == (int)secondSide) && ((int)secondSide == (int)thirdSide)) {
             System.out.println("this is a valid triangle");
         }
         else System.out.println("it's not a triangle");
